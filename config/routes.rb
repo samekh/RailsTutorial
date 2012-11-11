bcca5d842f24c8186551b34b50ecdc2c680e23d8
Dustin Dexter <dustindexter@gmail.com>
2012-11-10 17:24:36 -0700

    Removed default page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 RailsTutorial::Application.routes.draw do
+  get "welcome/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -9,7 +11,7 @@ RailsTutorial::Application.routes.draw do
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
-
+root :to => 'welcome#index'
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
